d28a53d131ff133ff5f9821201913bae7a5dffcd
t4 <tolma488@gmail.com>
2019-01-22 15:09:12 +0300

    small cleanups

--- Source.cpp
+++ Source.cpp
@@ -2,10 +2,7 @@
 #include <stdint.h>
 #include <windows.h>
 
-#define JUMP_SZ 200
-
-void taperewind(int blocksize);
-void taperewind_test(int blocksize, int offset);
+void taperewind_test(int blocksize, uint64_t start_offset, uint64_t stop_offset);
 void initdrive(char* drivename, int blocksize);
 
 HANDLE handle;
@@ -22,23 +19,21 @@ void geterror()
 int main(int argc, char** argv)
 {
 	if (argc != 5) {
-		printf("Error: specify drive name, block-size, command and and the offset on tape. Also make sure that medium is loaded to drive.\n");
-		printf("Example: tapetest.exe \"\\\\.\\Tape0\" 262144 rewind 107374182400\n");
+		printf("Error: specify drive name, block-size, start- and the stop-offset on tape. Also make sure that medium is loaded to drive.\n");
+		printf("Example: tapetest.exe \"\\\\.\\Tape0\" 262144 107374182400 107374182800\n");
 		return -1;
 	}
 
 	char* drivename = argv[1];
 	int blocksize = atoi(argv[2]);
-	char* cmd = argv[3];
-	uint64_t datasize = _atoi64(argv[4]);
+	uint64_t start_offset = _atoi64(argv[3]);
+	uint64_t stop_offset = _atoi64(argv[4]);
 
-	printf("Input parameters: %s %d %s %I64d\n", drivename, blocksize, cmd, datasize);
+	printf("Input parameters: %s %d %I64d %I64d\n", drivename, blocksize, start_offset, stop_offset);
 	initdrive(drivename, blocksize);
 
-	if (strcmp(cmd, "rewind") == 0) {
-		printf("Rewinding the tape...\n");
-		taperewind_test(blocksize, datasize);
-	}
+	printf("Rewinding the tape...\n");
+	taperewind_test(blocksize, start_offset, stop_offset);
 	CloseHandle(handle);
 	return 0;
 }
@@ -52,8 +47,7 @@ void initdrive(char* drivename, int blocksize)
 	}
 }
 
-void taperewind(int blocksize)
-{
+void taperewind_test(int blocksize, uint64_t start_offset, uint64_t stop_offset) {
 	TAPE_SET_MEDIA_PARAMETERS params;
 	memset(&params, 0, sizeof(params));
 	params.BlockSize = (DWORD)blocksize;
@@ -64,34 +58,13 @@ void taperewind(int blocksize)
 		CloseHandle(handle);
 		exit(-3);
 	}
-	res = SetTapePosition(handle, TAPE_REWIND, 0, 0, 0, FALSE);
-	if (NO_ERROR != res) {
-		printf("Tape positioning error.\n");
-		geterror();
-		CloseHandle(handle);
-		exit(-5);
-	}
-}
-
-void taperewind_test(int blocksize, int offset) {	
-	TAPE_SET_MEDIA_PARAMETERS params;
-	memset(&params, 0, sizeof(params));
-	params.BlockSize = (DWORD)blocksize;
-	DWORD res = SetTapeParameters(handle, SET_TAPE_MEDIA_INFORMATION, &params);
-	if (NO_ERROR != res) {
-		printf("Failed to set blocksize.\n");
-		geterror();
-		CloseHandle(handle);
-		exit(-3);
-	}
-	uint64_t jump_start = offset - JUMP_SZ;
-	uint32_t sizeLow;
-	uint32_t sizeHigh;
-	if (jump_start > 0) {
-		sizeLow = jump_start & 0xffffffff;
-		sizeHigh = jump_start >> 32;
-		printf("Rewinding to %I64d.\n", jump_start);
-		res = SetTapePosition(handle, TAPE_LOGICAL_BLOCK, 0, sizeLow, sizeHigh, FALSE);
+	uint32_t dwOffsetLow;
+	uint32_t dwOffsetHigh;
+	if (start_offset > 0) {
+		dwOffsetLow = start_offset & 0xffffffff;
+		dwOffsetHigh = start_offset >> 32;
+		printf("Rewinding to %I64d.\n", start_offset);
+		res = SetTapePosition(handle, TAPE_LOGICAL_BLOCK, 0, dwOffsetLow, dwOffsetHigh, FALSE);
 		if (NO_ERROR != res) {
 			printf("Tape positioning error.\n");
 			geterror();
@@ -99,10 +72,10 @@ void taperewind_test(int blocksize, int offset) {
 			exit(-5);
 		}
 	}
-	sizeLow = offset & 0xffffffff;
-	sizeHigh = offset >> 32;
-	printf("Rewinding to %I64d.\n", offset);
-	res = SetTapePosition(handle, TAPE_ABSOLUTE_BLOCK, 0, sizeLow, sizeHigh, FALSE);
+	dwOffsetLow = stop_offset & 0xffffffff;
+	dwOffsetHigh = stop_offset >> 32;
+	printf("Rewinding to %I64d.\n", stop_offset);
+	res = SetTapePosition(handle, TAPE_ABSOLUTE_BLOCK, 0, dwOffsetLow, dwOffsetHigh, FALSE);
 	if (NO_ERROR != res) {
 		printf("Tape positioning error.\n");
 		geterror();
